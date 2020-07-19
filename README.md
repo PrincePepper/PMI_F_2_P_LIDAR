8ffc2bc6d120809ce3c290cc17e2d7ca90b0edfe
PrincePepper <semyon3336@gmail.com>
2020-07-19 18:46:07 +1000

    #Final version Implemented a median filter for the average speed over time. Implemented the choice of actions with an even step of the median filter Accepts a matrix of any size

--- README.md
+++ README.md
@@ -1,8 +1,47 @@
 # PMI_F_2_P_LIDAR
----------
+
 This is the median filter for Lidar that accepts strings as input. 
 
 ```one line input = one lidar snapshot.```
+
+### work with filter
+```
+balance = Median_filter(step,{True from the necessity})
+balance.update([10, 8, 30, 5])
+```
+
+### Sample input:
+
+#### IN:
+```
+a = [[10, 8, 30, 5],
+     [8, 7, 40, 3],
+     [11, 10, 20, -3],
+     [10, 15, 50, 2],
+     [12, -3, 30, 1]]
+
+balance.update(a[i], 5])
+...
+```
+#### OUT:
+```
+[10, 8, 30, 5]
+[10, 8, 40, 5]
+[10, 8, 30, 3]
+[10, 10, 40, 3]
+[10, 8, 30, 2]
+```
+
+## __OR__
+Reading data through a file, but this requires deleting:
+```
+a = [[10, 8, 30, 5],
+     [8, 7, 40, 3],
+     [11, 10, 20, -3],
+     [10, 15, 50, 2],
+     [12, -3, 30, 1]]
+```
+---------
 
 ***The project was released for my University course***
 
--- main.py
+++ main.py
@@ -1,31 +1,55 @@
+import random
+
 class Median_filter:
     def __init__(self, _step: int, position=False):
         self._matrix = []  # Список всех входных строк
         self.step = _step  # Шаг медианного фильтра
         self.position = position  # Определяет что выводить при четном шаге медианного фильтра
 
-    @staticmethod
-    def average_of_2(_a: int, _b: int):  # Вычисляет среднее арифметическое среди 2-х элементов
-        return int((_a + _b) / 2)
+    def quickselect_median(self, l, pivot_fn=random.choice):
+        if self.position and len(l) % 2 == 0:
+            return 0.5 * (self.quickselect(l, int(len(l) / 2) - 1, pivot_fn) +
+                          self.quickselect(l, int(len(l) / 2), pivot_fn))
+        else:
+            return self.quickselect(l, int(len(l) / 2), pivot_fn)
 
-    def middle_more_than_3(self, _column: int):  # Вычисляет медианный элемент при шаге > 3
+    def quickselect(self, l, k, pivot_fn):
+        """
+        Выбираем k-тый элемент в списке l (с нулевой базой)
+        :param l: список числовых данных
+        :param k: индекс
+        :param pivot_fn: функция выбора pivot, по умолчанию выбирает случайно
+        :return: k-тый элемент l
+        """
+        if len(l) == 1:
+            assert k == 0
+            return l[0]
+
+        pivot = pivot_fn(l)
+
+        lows = [el for el in l if el < pivot]
+        highs = [el for el in l if el > pivot]
+        pivots = [el for el in l if el == pivot]
+
+        if k < len(lows):
+            return self.quickselect(lows, k, pivot_fn)
+        elif k < len(lows) + len(pivots):
+            # Нам повезло и мы угадали медиану
+            return pivots[0]
+        else:
+            return self.quickselect(highs, k - len(lows) - len(pivots), pivot_fn)
+
+    def middle_value(self, _column: int):
         _temp = 0
         _count = 0
         _buffer = []
         for newVal in self._matrix:
             if _count < self.step:
                 _buffer.append(newVal[_column])
-                _temp_s = sorted(_buffer)
-                if (_count + 1) % 2 == 0 and self.position:
-                    _temp = self.average_of_2(_temp_s[int(_count / 2)], _temp_s[int((_count + 1) / 2)])
-                else:
-                    _temp = _temp_s[int((_count + 1) / 2)]
+                _temp = self.quickselect_median(_buffer)
             else:
                 _buffer[_count % self.step] = newVal[_column]
-                if self.step % 2 == 0 and self.position:
-                    _temp = self.average_of_2(_buffer[int(self.step / 2)], _buffer[int((self.step - 1) / 2)])
-                else:
-                    _temp = sorted(_buffer)[int(self.step / 2)]
+                _temp = self.quickselect_median(_buffer)
             _count += 1
         return _temp
 
@@ -38,22 +62,33 @@ class Median_filter:
             if len(self._matrix) == 1:  # Проверка на первую входную последовательность
                 _temp.append(n)
             else:
-                _temp.append(self.middle_more_than_3(_column))
+                _temp.append(self.middle_value(_column))
         return _temp  # Возвращает готовую последовательнсть в зависимости от вх. данных
 
+
+"""
+    2 способа ввода:
+    - через файл
+    - через список
+    
+    Если хочешь через файл, следует удалить:
+     a = [[10, 8, 30, 5],
+          [8, 7, 40, 3],
+          [11, 10, 20, -3],
+          [10, 15, 50, 2],
+          [12, -3, 30, 1]]
+"""
+lines = [line.rstrip('\n') for line in open('input.txt')]
+a = []
+for i in lines:
+    a.append([int(j) for j in i.split()])
 
 a = [[10, 8, 30, 5],
      [8, 7, 40, 3],
      [11, 10, 20, -3],
      [10, 15, 50, 2],
      [12, -3, 30, 1]]
+
 balance = Median_filter(5)
 for i in a:
     print(balance.update(i))
-
-# результат при step(d) = 5
-# [10, 8, 30, 5]
-# [10, 8, 40, 5]
-# [10, 8, 30, 3]
-# [10, 10, 40, 3]
-# [10, 8, 30, 2]
